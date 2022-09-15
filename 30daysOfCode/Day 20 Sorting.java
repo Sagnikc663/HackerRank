c07dd3c57a783bc642a88cde7d623db4bcc184c9
Sagnik Chakraborty <65452981+Sagnikc663@users.noreply.github.com>
2022-09-15 17:48:57 +0530

    Add files via upload

---
+++ 30daysOfCode/Day 20 Sorting.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Solution{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int count =0;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                    count++;                    
+                }
+            }
+        }
+        System.out.println("Array is sorted in "+count+" swaps.");
+        System.out.println("First Element: "+arr[0]);
+        System.out.println("Last Element: "+arr[n-1]);        
+        
+    }
+}
